eb57f993f81d3a302c44ca5d3429502c41b4f5c3
Marcos Rodrigues <marcosrodrigues3546@gmail.com>
2023-08-27 13:37:28 -0300

    Adicionando agenda PHP
    Agenda de contatos desenvolvida em php e mysql

---
+++ AGENDA/create.php
@@ -0,0 +1,27 @@
+
+<?php
+    include_once("templates/header.php");
+?>
+   <div class="container">
+        <?php include_once("templates/backbtn.php")  ?>
+        <h1 id="main-title">Criar Contato</h1>
+        <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="post">
+            <input type="hidden" name="type" value="create">
+            <div class="form-group">
+                <label for="name">Nome do contato</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" require>
+            </div>
+            <div class="form-group">
+                <label for="phone">Telefone do contato</label>
+                <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone" require>
+            </div>
+            <div class="form-group">
+                <label for="observations">Observações</label>
+                <textarea type="text" class="form-control" id="observations" name="observations" placeholder="Insira a observações" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary mt-3">Cadastrar</button>
+        </form>
+   </div>
+<?php
+    include_once("templates/footer.php");
+?>
---
+++ AGENDA/edit.php
@@ -0,0 +1,31 @@
+
+<?php
+    include_once("templates/header.php");
+?>
+   <div class="container">
+        <?php include_once("templates/backbtn.php")  ?>
+        <h1 id="main-title">Editar Contato</h1>
+        <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="post">
+            <input type="hidden" name="type" value="edit">
+            <input type="hidden" name="id" value="<?= $contact['id'] ?>">
+            <div class="form-group">
+                <label for="name">Nome do contato</label>
+                <input type="text" class="form-control" id="name" name="name" 
+                placeholder="Digite o nome" require value="<?= $contact['name'] ?>">
+            </div>
+            <div class="form-group">
+                <label for="phone">Telefone do contato</label>
+                <input type="text" class="form-control" id="phone" name="phone" 
+                placeholder="Digite o telefone" require value="<?= $contact['phone'] ?>">
+            </div>
+            <div class="form-group">
+                <label for="observations">Observações</label>
+                <textarea type="text" class="form-control" id="observations" name="observations"
+                 placeholder="Insira a observações" rows="3"><?= $contact['observations'] ?></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary mt-3">Atualizar</button>
+        </form>
+   </div>
+<?php
+    include_once("templates/footer.php");
+?>
